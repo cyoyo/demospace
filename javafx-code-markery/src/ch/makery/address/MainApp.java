b7abfcf8429fd8f55af55efb9fb1b2267dc28f7e
Yoyo <hw.cyoyo@gmail.com>
2018-05-06 23:25:52 +0800

    add project javafx-code-markery

---
+++ javafx-code-markery/src/ch/makery/address/MainApp.java
@@ -0,0 +1,116 @@
+package ch.makery.address;
+
+import java.io.IOException;
+
+import ch.makery.address.model.Person;
+import ch.makery.address.view.PersonOverviewController;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+	
+	private Stage primaryStage;
+	
+    private BorderPane rootLayout;
+    
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+    
+    /**
+     * Constructor
+     */
+    public MainApp() {
+        // Add some sample data
+        personData.add(new Person("Hans", "Muster"));
+        personData.add(new Person("Ruth", "Mueller"));
+        personData.add(new Person("Heinz", "Kurz"));
+        personData.add(new Person("Cornelia", "Meier"));
+        personData.add(new Person("Werner", "Meyer"));
+        personData.add(new Person("Lydia", "Kunz"));
+        personData.add(new Person("Anna", "Best"));
+        personData.add(new Person("Stefan", "Meier"));
+        personData.add(new Person("Martin", "Mueller"));
+    }
+
+	/** 
+	 *  这里的 Stage 是一个主容器，它就是我们通常所认为的窗口<br>
+	 *  在这个 Stage 里面，我们可以放置一个 Scene，也可以切换别的 Scene，<br>
+	 *  而在这个 Scene 里面，我们就可以放置各种各样的控件<br>
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("AddressApp");
+		
+		initRootLayout();
+
+        showPersonOverview();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	/**
+	 * Initializes the root layout.
+	 */
+	public void initRootLayout() {
+		try {
+			// Load root layout from fxml file.
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			
+			// Show the scene containing the root layout.
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Shows the person overview inside the root layout.
+	 */
+	public void showPersonOverview() {
+		try {
+			// Load person overview.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Set person overview into the center of root layout.
+            rootLayout.setCenter(personOverview);
+            
+            // Give the controller access to the main app.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Returns the main stage.
+	 * 
+	 * @return
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+	
+	/**
+     * Returns the data as an observable list of Persons. 
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+}
